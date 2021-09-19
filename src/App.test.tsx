b5ae27bc6d5250f2a0e5e649f696a50c40cd8294
Ed <61915524+e2r2fx@users.noreply.github.com>
2021-09-19 12:41:49 -0400

    Render a list of 100 elements

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
 describe('<App/>', () => {
-  test('should be true', () => {
-    expect(true).toBe(true);
+  test('should render a list of 100 elements', async () => {
+    const list = await render(<App />).findAllByTestId('list-element');
+    expect(list.length).toBe(100);
   });
 });
-
-export {};
--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,10 @@
-const App = (): JSX.Element => <></>;
+const App = (): JSX.Element => {
+  const list: JSX.Element[] = [];
+
+  for (let i = 1; i <= 100; i++) {
+    list.push(<div data-testid="list-element" key={i} />);
+  }
+
+  return <>{list}</>;
+};
 export default App;
