84ae171cd204ccb661f4abbde35338ff5e5d2e8b
Ed <61915524+e2r2fx@users.noreply.github.com>
2021-09-19 12:54:13 -0400

    FizzBuzz logic implemented

--- src/components/FizzBuzz/FizzBuzz.tsx
+++ src/components/FizzBuzz/FizzBuzz.tsx
@@ -4,10 +4,12 @@ interface IProps {
 
 const FizzBuzz = ({ number }: IProps): JSX.Element => {
   let fizzedNum: string | number = '';
-  if (number === 3) fizzedNum = 'Fizz';
-  if (number === 5) fizzedNum = 'Buzz';
-  if (number === 15) fizzedNum = 'FizzBuzz';
-  if (number === 7) fizzedNum = 7;
+  const isMultipleOf3 = number % 3 === 0;
+  const isMultipleOf5 = number % 5 === 0;
+
+  if (isMultipleOf3) fizzedNum = 'Fizz';
+  if (isMultipleOf5) fizzedNum += 'Buzz';
+  if (!isMultipleOf3 && !isMultipleOf5) fizzedNum = number;
 
   return <div>{fizzedNum}</div>;
 };
