a75d8dc66dbd1a6a94e27b039f51b25e5acd9c39
Ed <61915524+e2r2fx@users.noreply.github.com>
2021-09-19 12:56:10 -0400

    More robust FizzBuzz tests

--- src/components/FizzBuzz/FizzBuzz.test.tsx
+++ src/components/FizzBuzz/FizzBuzz.test.tsx
@@ -5,20 +5,36 @@ describe('<FizzBuzz/>', () => {
   test('should render Fizz for multiples of 3', async () => {
     const component = render(<FizzBuzz number={3} />);
     expect(await component.findByText('Fizz')).toBeInTheDocument();
+    component.rerender(<FizzBuzz number={6} />);
+    expect(await component.findByText('Fizz')).toBeInTheDocument();
+    component.rerender(<FizzBuzz number={9} />);
+    expect(await component.findByText('Fizz')).toBeInTheDocument();
   });
 
   test('should render Buzz for multiples of 5', async () => {
     const component = render(<FizzBuzz number={5} />);
+    expect(await component.findByText('Buzz')).toBeInTheDocument();
+    component.rerender(<FizzBuzz number={10} />);
+    expect(await component.findByText('Buzz')).toBeInTheDocument();
+    component.rerender(<FizzBuzz number={20} />);
     expect(await component.findByText('Buzz')).toBeInTheDocument();
   });
 
   test('should render FizzBuzz for multiples of 3 and 5', async () => {
     const component = render(<FizzBuzz number={15} />);
     expect(await component.findByText('FizzBuzz')).toBeInTheDocument();
+    component.rerender(<FizzBuzz number={30} />);
+    expect(await component.findByText('FizzBuzz')).toBeInTheDocument();
+    component.rerender(<FizzBuzz number={45} />);
+    expect(await component.findByText('FizzBuzz')).toBeInTheDocument();
   });
 
   test('should render number for non multiples of 3 and 5', async () => {
     const component = render(<FizzBuzz number={7} />);
     expect(await component.findByText(7)).toBeInTheDocument();
+    component.rerender(<FizzBuzz number={13} />);
+    expect(await component.findByText(13)).toBeInTheDocument();
+    component.rerender(<FizzBuzz number={22} />);
+    expect(await component.findByText(22)).toBeInTheDocument();
   });
 });
